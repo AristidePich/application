6051cb80c04ebb60a7149990e93e8c513102d48b
AristidePich <aristide.pichereau@gmail.com>
2022-09-24 22:03:27 +0200

    first

---
+++ lib/auth.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:realtime_chat/helperfun/sharedpref_helper.dart';
+import 'package:realtime_chat/services/database.dart';
+import 'package:realtime_chat/views/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
